28eb4d3506686b7ca65f65aabe146def1200165d
Wasenshi <recka123@gmail.com>
2022-03-23 22:42:44 +0700

    Init UpdateServer project

---
+++ UpdateServer/Controllers/UpdateController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace UpdateServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UpdateController : ControllerBase
+    {
+
+        private readonly ILogger<UpdateController> _logger;
+
+        public UpdateController(ILogger<UpdateController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            string version = fvi.FileVersion;
+
+            return Ok($"server version: {version}");
+        }
+    }
+}
